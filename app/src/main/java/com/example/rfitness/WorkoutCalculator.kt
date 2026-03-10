2b57cbdd84f27983a770fabe359aa09fb81170d7
Brobe <robertofeldt@hotmail.com>
2026-03-10 13:14:31 +0100

    added round function to power exercises, might work as intended have NOT tested

--- app/src/main/java/com/example/rfitness/WorkoutCalculator.kt
+++ app/src/main/java/com/example/rfitness/WorkoutCalculator.kt
@@ -5,11 +5,27 @@ object WorkoutCalculator {
     fun weightForExercise(exercise: Exercise): Double? =
         when (exercise) {
             is Exercise.PowerExercise ->
-                exercise.oneRepMax * exercise.percentage
+                getCorrectWeight(exercise.oneRepMax * exercise.percentage)
 
             is Exercise.GvtExercise ->
                 exercise.weight
 
         }
+
+    private fun getCorrectWeight(percentageWeight: Double): Double? {
+        val modWeight = percentageWeight % 10
+
+        if (modWeight == 0.0)
+            return percentageWeight
+        else if (0 < modWeight && modWeight <= 2.5) 
+            return (percentageWeight - modWeight) + 2.5
+        else if (2.5 < modWeight && modWeight <= 5.0)
+            return (percentageWeight - modWeight) + 5
+        else if (5 < modWeight && modWeight <= 7.5)
+            return (percentageWeight - modWeight) + 7.5
+        else 
+            return (percentageWeight - modWeight) + 10.0
+
+    }
 
 }
